9403fa56e62b2e287f8de17239db36962676c8a5
Fabian <156524829+fabianbola@users.noreply.github.com>
2024-09-30 16:59:37 -0400

    first progress

---
+++ Assignment.1/app01/src/components/Card.js
@@ -0,0 +1,18 @@
+import servicePic from '../assets/Service.Devwebsites.jpg'
+
+function card(){
+
+    return(
+        <div className="card">
+            <img className='card-image' src={servicePic} alt="Card picture"></img>
+            <h2 className='card-tittle'>Tittle</h2>
+            <p className='card-description'>Description</p>
+
+        </div>
+
+
+    )
+
+}
+
+export default card
