99fac0d34c4b102d66876030f18b85a9b2195811
Fabian <156524829+fabianbola@users.noreply.github.com>
2024-09-30 23:43:47 -0400

    Update Header.js

--- Assignment.1/app03/src/components/Layout/Header.js
+++ Assignment.1/app03/src/components/Layout/Header.js
@@ -1,3 +1,4 @@
+import { Outlet, NavLink, Link } from "react-router-dom";
 import logo from '../../assets/Logo.jpg'
 
 function Headers(){
@@ -6,12 +7,45 @@ function Headers(){
             <center>
             <img className='logo' src={logo} alt="Logo image"></img>
             </center>
-            <div className='topnav'>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            
+
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  <i className="fas fa-home"> </i> Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">
+                  <i className="fa-solid fa-address-book"></i> About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/services">
+                  <i className="fa-solid fa-address-book"></i> Service
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/projects">
+                  <i className="fas fa-project-diagram"></i> Projects
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contactme">
+                  <i className="fa-solid fa-address-book"></i> Contact me
+                </NavLink>
+              </li>
+
+            </ul>            
+          </div>
+
+            {/* <div className='topnav'>
                 <a className="active" href="#home">Home</a>
                 <a href="#news">News</a>
                 <a href="#contact">Contact</a>
                 <a href="#about">About</a>
-            </div>
+            </div> */}
         </>
 
     );
